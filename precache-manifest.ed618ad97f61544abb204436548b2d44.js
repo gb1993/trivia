13d05a70d0f28f34480dc1b85cd6b66e1035317a
Gabriel Branco <gbanco1993@gmail.com>
2022-06-02 00:28:26 -0300

    Updates

---
+++ precache-manifest.ed618ad97f61544abb204436548b2d44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ccb05246864d63535239dd6736e2c8",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "1a999b0a1e7c3dc4b238",
+    "url": "/trivia/static/css/main.3797c3b8.chunk.css"
+  },
+  {
+    "revision": "534ea6c20fa4d9bc796b",
+    "url": "/trivia/static/js/2.061659c2.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia/static/js/2.061659c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a999b0a1e7c3dc4b238",
+    "url": "/trivia/static/js/main.f02160a5.chunk.js"
+  },
+  {
+    "revision": "13da866554f636ecdfac",
+    "url": "/trivia/static/js/runtime-main.62eca463.js"
+  },
+  {
+    "revision": "691c04a1cc051bb0c513f53a1349962e",
+    "url": "/trivia/static/media/happy.691c04a1.png"
+  },
+  {
+    "revision": "2a22123f953120b56a506eed1ddc39db",
+    "url": "/trivia/static/media/loadingLogo.2a22123f.gif"
+  },
+  {
+    "revision": "83dccd659a7235545e53da82293055d6",
+    "url": "/trivia/static/media/sad.83dccd65.png"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
